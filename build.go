d7cbdd7663f7cd51216820249b8bc7fde38fb54f
mewmew <random0x00@gmail.com>
2012-10-18 20:19:01 +0200

    Added CanBuild status. Second commit.

---
+++ build.go
@@ -0,0 +1,16 @@
+package status
+
+import "fmt"
+import "os/exec"
+
+// Build compiles the package and updates it's CanBuild status.
+func (pkg *Package) Build() (err error) {
+	pkg.CanBuild = false
+	cmd := exec.Command("go", "build", pkg.Path)
+	err = cmd.Run()
+	if err != nil {
+		return fmt.Errorf("Build failed for: %s", pkg.Path)
+	}
+	pkg.CanBuild = true
+	return nil
+}
